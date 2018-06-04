51d04109169c2e020150bb01b28b7a47eb2112d7
Silap Aliyev <sylapaliyev@gmail.com>
2018-06-05 06:31:32 +0900

    i dont even

--- detection.py
+++ detection.py
@@ -1,23 +1,17 @@
 import numpy as np
 import argparse
+import sys
 import cv2
 import imutils
 
-# GREEN
-# lower = (0,    83-5, 128-5)
-# upper = (255, 103+5, 143+5)
-
-# YELLOW
-# lower = (0,   -15+128, 20+128)
-# upper = (255,   5+128, 40+128)
-
-# RED
-# lower = (0,   40+128, 20+128)
-# upper = (255, 75+128, 50+128)
-
-# BLUE
-# lower = (0,   -20+128, -55+128)
-# upper = (255,   5+128, -20+128)
+'''
+bounds = {
+  'green':  [(0, -50+128,  -5+128), (255, -20+128,  20+128)],
+  'yellow': [(0, -15+128,  20+128), (255,   5+128,  40+128)],
+  'red':    [(0,  40+128,  20+128), (255,  75+128,  50+128)],
+  'blue':   [(0, -20+128, -55+128), (255,   5+128, -20+128)],
+}
+'''
 
 # (BAD) ORANGE
 # lower = (0,    6+128, 10+128)
@@ -37,12 +31,19 @@ def detect(image):
   
   # thresholding
   bounds = {
-    "green":  [(0,    83-5, 128-5),     (255, 103+5, 143+5)],
-    "yellow": [(0,   -15+128, 20+128),  (255,   5+128, 40+128)],
-    "red":    [(0,   35+128,  5+128),   (255,  75+128, 50+128)],
-    "blue":   [(0,   -20+128, -55+128), (255,   5+128, -20+128)],
-    # "cyan":   [(50,   -15+128, -5+128),  (200,   0+128,  5+128)],
+    'green':  [(0, -50+128,  -5+128), (255, -20+128,  20+128)],
+    'yellow': [(0, -15+128,  20+128), (255,   5+128,  40+128)],
+    'red':    [(0,  40+128,  20+128), (255,  75+128,  50+128)],
+    'blue':   [(0, -20+128, -55+128), (255,   5+128, -20+128)],
   }
+
+  # bounds = {
+  #   "green":  [(10, -30+128, -10+128), (235, -5+128,  10+128)],
+  #   "yellow": [(20, -5+128,  20+128), (235,   5+128,  40+128)],
+  #   "red":    [(10,  12+128,   5+128), (140,  45+128,  40+128)], # done
+  #   "blue":   [(20, -20+128, -55+128), (235,   5+128, -20+128)],
+  #   # "cyan":   [(50,   -15+128, -5+128),  (200,   0+128,  5+128)],
+  # }
 
   image = cv2.GaussianBlur(image, (5, 5), 0)
   lab_image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
@@ -63,7 +64,7 @@ def detect(image):
         continue
 
       # ignore small fragmentations
-      if M["m00"] < 20:
+      if M["m00"] < 100:
         # print('area <20 ignored')
         continue
 
@@ -81,8 +82,8 @@ def detect(image):
       #   print('<green> area:', M["m00"])
 
       # Draw shape
-      # cv2.drawContours(image, [approx], -1, (0, 255, 0), 2)
-      cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+      cv2.drawContours(image, [approx], -1, (0, 0, 255), 1)
+      cv2.drawContours(image, [c], -1, (0, 255, 0), 1)
       cv2.circle(image, (cX, cY), 3, (255, 255, 255), -1)
       cv2.putText(image, label, (cX+5, cY), cv2.FONT_HERSHEY_SIMPLEX, .3, (140, 174, 32), 1)
 
@@ -98,10 +99,17 @@ def detect(image):
         sodas.append((cX, cY))
 
   # return image
-  return (robfront, robback), corners, sodas, milks
+  return (robfront, robback), corners, sodas, milks, image
 
 def main():
-  cap = cv2.VideoCapture(1)
+  if len(sys.argv) > 1:
+    image = cv2.imread(sys.argv[1])
+    (robfront, robback), corners, sodas, milks, res_image = detect(image)
+    cv2.imshow("detection", res_image)
+    cv2.waitKey(0)
+    return
+
+  cap = cv2.VideoCapture(0)
 
   while True:
     # Read image from frame
@@ -110,7 +118,7 @@ def main():
       print("Couldn't read capture data")
       break
 
-    res_image = detect(image)
+    (robfront, robback), corners, sodas, milks, res_image = detect(image)
 
     # Show frame
     cv2.imshow("detection", res_image)
